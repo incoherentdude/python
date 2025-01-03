b3cd0bdf55f97310e14d3d38a02d58150093aef1
sumanth.ponna <sumanth.ponna3@gmail.com>
2025-01-03 14:00:36 +0530

    Docker & files

--- app.py
+++ app.py
@@ -40,5 +40,5 @@ def calc():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000) # EC2 on AWS
+    app.run(host="0.0.0.0",port=80) # EC2 on AWS
     #app.run(host="127.0.0.1",port=8080) # local machine
